3bd6cfb49a72edde6b1b6686864133913a067e54
miguelangelrm <miguelangelrm31@gmail.com>
2022-07-01 02:10:06 +0200

    dark

--- config/theme.ts
+++ config/theme.ts
@@ -2,7 +2,7 @@ import { extendTheme, ThemeConfig } from '@chakra-ui/react'
 import { palette } from './constants'
 
 const config: ThemeConfig = {
-  initialColorMode: 'light',
+  initialColorMode: 'dark',
   useSystemColorMode: true,
 }
 
